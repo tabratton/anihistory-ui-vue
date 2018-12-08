f6ab34b6a380f383f1f3a3a3d68948c57453db43
Tyler Bratton <tylerbratton96@gmail.com>
2018-12-08 18:05:25 -0500

    Add vue-cli-plugin-s3-deploy

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,26 @@ module.exports = {
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: false
+    },
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      region: 'us-east-1',
+      bucket: 'anihistory.moe',
+      createBucket: false,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: true,
+      cloudfrontId: 'E3RJ815PE513Z4',
+      cloudfrontMatchers: '/*',
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0'
     }
   },
   configureWebpack: config => {
