177165fd003cc25a8df6677fa1bd289bc1c356d8
Tyler Bratton <tylerbratton96@gmail.com>
2021-03-13 15:27:38 -0500

    put important line back

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -127,6 +127,8 @@ export default {
       list.forEach(listElement => {
         // Check each row for each list element to make sure all possible gaps are filled
         rows.forEach((row, index) => {
+          if (listElement.category) return
+          
           const length = row.length
 
           // If there's no other elements there can't be date range conflicts
